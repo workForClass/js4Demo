99151eb59d664474f54cf2496e4986e81cb9d575
Lisa Ostman <peolina@live.com.au>
2021-09-21 12:04:55 +1000

    updated

--- ans/fetch.js
+++ ans/fetch.js
@@ -5,11 +5,16 @@ const getData = () => {
   fetch("https://reqres.in/api/users?page=2")
     .then(
       response => {
+        // console log response, its a Response Object
         if (response.ok) {
+          // takes response's body and makes it into js object, returns promise
           return response.json();
+        } else {
+          // if reaches server but response back is not ok
+          throw new Error("Request failed!");
         }
-        throw new Error("Request failed!");
       },
+      // network error if fetch rejects: can't reach server at all (e.g url incorrect)
       networkError => {
         console.log(networkError.message);
       }
@@ -23,9 +28,11 @@ const getData = () => {
 const sendData = () => {
   fetch("https://reqres.in/api/register", {
     method: "POST",
+    // lets server know the body is the form of json
     headers: {
       "Content-type": "application/json",
     },
+    // data we send to the server. will be different depending on the data.
     body: JSON.stringify({ email: "eve.holt@reqres.in", password: "pistol" }),
   })
     .then(
